5d3d474b5036f06b850c7642f2c133793103b5c5
aishik-pyne <aishikpyne@gmail.com>
2018-11-17 15:31:13 +0530

    partial commit

--- networks/lstm.py
+++ networks/lstm.py
@@ -1,4 +1,16 @@
-import tensorflow as tf
-from tensorflow.keras.applications.resnet50 import *
-model = ResNet50(include_top=False)
-model.ad
+from keras.applications import VGG16
+from keras.models import Model, Sequential
+from keras.layers import LSTM, Dense, Flatten
+class LstmModels:
+  def __init__(self, baseModel='vgg16'):
+    if baseModel == 'vgg16':
+      self.baseModel = VGG16(include_top=False, weights='imagenet', input_shape=(120, 160, 3), classes=6)
+    
+
+  def extractClipFeatures(self, images):
+    pass
+
+  def cnnLstm(self):
+    model = Sequential()
+
+LstmModels()
