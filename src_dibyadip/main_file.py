90712bc381ab08df8d39f1ba761504f70966cf5c
Dibyadip Chatterjee <chatdip98.dc@gmail.com>
2019-02-05 23:32:56 +0530

    Fix feature dimension error

--- src_dibyadip/main_file.py
+++ src_dibyadip/main_file.py
@@ -1,20 +1,20 @@
 from __future__ import division
-from decimal import Decimal
-from skimage import exposure
-from skimage import feature
-from shapely.geometry import Point
-from shapely.geometry import Polygon
-from trackbody import trackbody, HOG, HOG_temporal
-from classifier import SVM, neural_net_classifier, desicion_tree_classifier
-from PIL import Image
+import os
 import pandas as pd
 import numpy as np 
 import cv2
 import imutils
 import scipy
 import math
-import os
 import sys
+from decimal import Decimal
+from skimage import exposure
+from skimage import feature
+from shapely.geometry import Point
+from shapely.geometry import Polygon
+from PIL import Image
+from classifier import SVM, desicion_tree_classifier, MLP
+from dopt import dense_flow
 
 if __name__=="__main__":
 	process = sys.argv[1]
@@ -23,46 +23,47 @@ if __name__=="__main__":
 		Flag = False
 		feature_vector = np.array([])
 		count = -1
-		#the root directory for the dataset is to be written as 'location/dataset_name'
-		rootDir = ''
+		rootDir = '/media/dibyadip/DC/Project Work/SKS/activity_recognition/KTH'
+		#rootDir = './Weizman/'
 
 		for dirName, subdirList, fileList in os.walk(rootDir):
-			vid=""
-			count+=1 
+
+			str = ""
+			count = count+1 
 			count1 = 0
 
 			is_first = True
 			for fname in fileList:
-				count1 = count1 + 1
-				vid = dirName+'/'+fname
 
-				b = trackbody(vid)
-				h = HOG_temporal(vid)
-				b = np.concatenate((b, h), axis=0)
-				
+				count1 = count1 + 1
+				str = dirName+'/'+fname
+				b = dense_flow(str)
+				#b.append(count)
 				count_final = np.array([count],dtype='int')
 
 				b = np.concatenate((b,count_final),axis=0)	
 
+				#print(b)
+	  
 				if Flag==False and not feature_vector:
 					feature_vector = np.array([b])
 					Flag=True
 					
 				else:
-					feature_vector = np.vstack((feature_vector,b))
+					if(b.shape[0]==feature_vector.shape[1]):
+						feature_vector = np.vstack((feature_vector,b))
 					
+				print(feature_vector.shape)
 				is_first=False
 
-				print(feature_vector.shape)
-				#if count1==2:
-				#	break
-				#feature_vector = np.matrix(feature_vector)
-
-		np.save('./data/feature_vector_HMDB_Hog_temp.npy', feature_vector)
-
-	#print(feature_vector)
+		print(feature_vector.shape)
+		np.save("./data/feature_vector_KTH_model.npy", feature_vector)
 	else:
-		feature_vector = np.load('./data/feature_vector_HMDB_Hog_temp.npy')
+		feature_vector = np.load('./data/feature_vector_KTH_9parts.npy')
 
+	#print(feature_vector.shape)
 	SVM(feature_vector)
 	#desicion_tree_classifier(feature_vector)
+	#MLP(feature_vector)
+	#feature_vector.dump("feature_matrix.dat")
+	#mat2 = numpy.load("my_matrix.dat")
